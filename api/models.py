fe12641f7cd072f07f182732f84808654b2e8748
harsh-9in <harshkumar9.in@gmail.com>
2021-03-25 00:02:37 +0530

    feat: Room API endpoint created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    code=models.CharField(max_length=8,unique=True,default="")
+    host=models.CharField(max_length=40,unique=True)
+    guest_can_pause=models.BooleanField(default=False,null=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.host
+
