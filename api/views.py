02dcb9e6264d53e58ea52cf98e2826c589f759d5
harsh-9in <harshkumar9.in@gmail.com>
2021-03-25 00:02:37 +0530

    feat: Room API endpoint created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Room
+from .serializers import RoomSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    serializer_class=RoomSerializer
+    queryset=Room.objects.all()
+
+
