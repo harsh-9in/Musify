0613b4e736f1e3f350f268274fcd71f26f20d50d
harsh-9in <harshkumar9.in@gmail.com>
2021-03-25 13:51:51 +0530

    react packages installed

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model=Room
-        fields=['code','host','guest_can_pause']
+        fields=['code','host','guest_can_pause','created_at']
